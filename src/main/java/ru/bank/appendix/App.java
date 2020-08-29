b233401a6b1031352dea4fd5f6486ac434f1a047
konstantin <kotmix@bk.ru>
2020-08-29 14:33:48 +0400

    Create User and App

--- src/main/java/ru/bank/appendix/App.java
+++ src/main/java/ru/bank/appendix/App.java
@@ -1,4 +1,13 @@
 package ru.bank.appendix;
 
 public class App {
+
+    private String ip;
+    private String port;
+    private String protocol;
+    private String server;
+
+    //Через CommandInput получаем данные от пользователя
+    //Вызываем CommandOperationPay
+    //Ожидает результат операции
 }
--- src/main/java/ru/bank/server/PayMobilePhone.java
+++ src/main/java/ru/bank/server/PayMobilePhone.java
@@ -1,4 +1,9 @@
 package ru.bank.server;
 
 public class PayMobilePhone implements Pay {
+
+    public void pay() {
+        System.out.println("Оплата произведена успешно");
+        // Производит оплату и сообщает Серверу результат
+    }
 }
