6c8b603e41813ceb78c5281459379742c50bb0b3
konstantin <kotmix@bk.ru>
2020-08-29 11:56:29 +0400

    Create User and App

--- src/main/java/ru/bank/server/Server.java
+++ src/main/java/ru/bank/server/Server.java
@@ -4,5 +4,4 @@ public class Server {
 
     // Server принимает данные c помощью CommandOperationPay и с помощью PayMobilePhone производит оплату
     // Возвращает результат об оплате в Приложение
-
 }
